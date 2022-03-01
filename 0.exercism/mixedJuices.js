8471beca582b3626903fe8a8409248b7b18df207
marcelofcruz <marcelocdcruz@gmail.com>
2022-02-28 22:23:23 -0300

    mixedJuice: fineshed

--- 0.exercism/mixedJuices.js
+++ 0.exercism/mixedJuices.js
@@ -23,4 +23,55 @@ const timeToMixJuice = juice => {
 
     return time;
 };
-console.log(timeToMixJuice('Green Garden'));
+
+const limesToCut = (wedgesNeeded, limes) => {
+    let lime = 0
+    let totalWedges = 0
+    if(wedgesNeeded !== 0 && limes.length !== 0){
+        while (totalWedges <= wedgesNeeded){
+            if (lime === limes.length && totalWedges<wedgesNeeded){
+                return lime;
+            }
+            switch (limes[lime]) {
+                case 'small':
+                    totalWedges+=6
+                    break;
+                case 'medium':
+                    totalWedges+=8
+                    break;
+                case 'large':
+                    totalWedges+=10
+                    break;
+                default:
+                    break;
+            }
+           
+            lime++
+        }
+        return lime;
+
+    } else {
+        return lime
+    }
+}
+
+const orders = [
+    'Tropical Island',
+    'Energizer',
+    'Limetime',
+    'All or Nothing',
+    'Pure Strawberry Joy'
+]
+
+const remainingOrders = (timeLeft, orders) => {
+    let count = 0
+    let timeToMix = 0
+    while (timeToMix < timeLeft){
+        timeToMix += timeToMixJuice(orders[count])
+        count++
+    }
+    
+    return orders.slice(count)
+}
+
+console.log(remainingOrders(7, orders))
